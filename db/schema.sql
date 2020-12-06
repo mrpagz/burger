78e4e2b8518d1e336a5310e9c108f860e97627be
Francis Roy Balasabas <francisroy1124@gmail.com>
2020-12-05 21:57:20 -0500

    added server codes

--- db/schema.sql
+++ db/schema.sql
@@ -4,11 +4,10 @@ CREATE DATABASE burger_db;
 
 USE burger_db;
 
-CREATE TABLE burger (
+CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   burger_name VARCHAR(30),
   devoured BOOLEAN DEFAULT FALSE,
   PRIMARY KEY(Id)
 )
 
-insert info wishes
